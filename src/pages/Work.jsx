77fb15d9c591a556c4ececd99b756a2aa9d14fa6
Prabal Bhandary <109888062+prabalbhandary@users.noreply.github.com>
2025-02-09 13:00:46 +0545

    Update Work.jsx

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const projects = [
-  { id: 1, title: 'Mak Movies Frontend', image: '/images/project-1.png', description: 'This is a movie app frontend build with Next JS.', link: 'https://movie-frontend-gray-phi.vercel.app/' },
-  { id: 2, title: 'Mak Movies Backend', image: '/images/project-2.png', description: 'This is a movie app backend build with Next JS.', link: 'https://movie-frontend-gray-phi.vercel.app/' },
-  { id: 3, title: 'Chat App', image: '/images/project-3.png', description: 'This is a chat app build with MERN stack.', link: 'https://chat-me-9912.onrender.com/' },
-  { id: 4, title: 'Book Store App', image: '/images/project-4.png', description: 'This is a book store app build with MERN stack.', link: 'https://bookstore-client-two.vercel.app/' },
-  { id: 5, title: 'Employee Management System', image: '/images/project-5.png', description: 'This is an Employee Management Syste App build with MERN stack.', link: 'https://employee-management-system-8n86.onrender.com/' }
+  { id: 1, title: 'Mak Movies Frontend', image: '/images/project-1.png', description: 'This is a movie app frontend build with Next JS.', link: 'https://movie-frontend-gray-phi.vercel.app/', link2: '#' },
+  { id: 2, title: 'Mak Movies Backend', image: '/images/project-2.png', description: 'This is a movie app backend build with Next JS.', link: 'https://movie-frontend-gray-phi.vercel.app/', link2: '#' },
+  { id: 3, title: 'Chat App', image: '/images/project-3.png', description: 'This is a chat app build with MERN stack.', link: 'https://chat-me-9912.onrender.com/', link2: '#' },
+  { id: 4, title: 'Book Store App', image: '/images/project-4.png', description: 'This is a book store app build with MERN stack.', link: 'https://bookstore-client-two.vercel.app/', link2: '#' },
+  { id: 5, title: 'Employee Management System', image: '/images/project-5.png', description: 'This is an Employee Management Syste App build with MERN stack.', link: 'https://employee-management-system-8n86.onrender.com/', link2: '#' }
 ];
 
 const Work = () => {
@@ -30,6 +30,9 @@ const Work = () => {
               <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600">
                 <a href={project.link} target="_blank" rel="noopener noreferrer">
                   View Project
+                </a>
+                <a href={project.link2} target="_blank" rel="noopener noreferrer">
+                  Buy Source Code
                 </a>
               </button>
             </div>
